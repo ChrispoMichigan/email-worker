ffc23f8ebda6147f2d28eaf66f52756c1ccd47e5
Chrispo <christiancastillo0350@gmail.com>
2026-06-13 06:58:25 -0700

    Redis url

--- src/queue.js
+++ src/queue.js
@@ -2,11 +2,13 @@ import { Queue } from "bullmq";
 import IORedis from "ioredis";
 import "dotenv/config";
 
-export const connection = new IORedis({
-  host: process.env.REDIS_HOST || "127.0.0.1",
-  port: Number(process.env.REDIS_PORT) || 6379,
-  maxRetriesPerRequest: null,
-});
+export const connection = process.env.REDIS_URL
+  ? new IORedis(process.env.REDIS_URL, { maxRetriesPerRequest: null })
+  : new IORedis({
+      host: process.env.REDIS_HOST || "127.0.0.1",
+      port: Number(process.env.REDIS_PORT) || 6379,
+      maxRetriesPerRequest: null,
+    });
 
 export const emailQueue = new Queue("email-queue", {
   connection,
